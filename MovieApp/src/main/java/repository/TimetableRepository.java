1c8500f2c9efd454c37080dfdd831e1b740288b3
Carol Li <ziqili33@gmail.com>
2022-05-23 10:03:35 +0100

    timetable repository done

--- MovieApp/src/main/java/repository/TimetableRepository.java
+++ MovieApp/src/main/java/repository/TimetableRepository.java
@@ -1,4 +1,6 @@
 package repository;
 
-public interface TimetableRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimetableRepository extends JpaRepository<Timetable, Long> {
 }
