789b6a4ae784aeae1ad7e028e2c4e09ce218dcbd
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-23 10:57:53 +0100

    added @getMapping to venue controller

--- MovieApp/src/main/java/controller/VenueController.java
+++ MovieApp/src/main/java/controller/VenueController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import model.Movie;
+import model.Venue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class VenueController {
+
+
+    @Autowired
+
+    private final VenueController venueController;
+
+    public VenueController(VenueController venueController) {
+        this.venueController = venueController;
+    }
+
+    @GetMapping("/venue/{name}")
+    public ResponseEntity<Venue> getName(@PathVariable String name) {
+        Venue venue = venueRepository.findByName(name);
+        return ResponseEntity
+                .ok()
+                .body(venue);
+    }
+
+    @GetMapping("/venue/{id}")
+        public void deleteVenue(@PathVariable Long id){
+        venueRepository.deleteById(id);
+    }
+
+
 }
