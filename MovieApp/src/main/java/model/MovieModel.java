941e72db0b3fc138186235a5e398acdbb340b3f0
hufengyi11 <fengyihu456@gmail.com>
2022-05-23 09:56:28 +0100

    added movie attributes

--- MovieApp/src/main/java/model/MovieModel.java
+++ MovieApp/src/main/java/model/MovieModel.java
@@ -1,4 +1,24 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class MovieModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int time_length;
+    private int rating;
+    private String genre;
+    private int price;
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JsonIgnoreProperties(value = {"movies"})
+    private Set<Venue> venues;
+
+
+
 }
