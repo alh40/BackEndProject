f22c1ad9fee5ae5800139e6e4d42d81687a45fe0
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-23 10:08:04 +0100

    Timetable model

--- MovieApp/src/main/java/model/TimetableModel.java
+++ MovieApp/src/main/java/model/TimetableModel.java
@@ -1,5 +1,54 @@
 package model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class TimetableModel {
-//jkbkjb
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String date;
+    private int time;
+
+
+    public TimetableModel() {
+    }
+
+    public TimetableModel(Long id, String date, int time) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+    }
+
+    enum days{
+        Monday, tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
 }
