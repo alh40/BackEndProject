fceab97d634b3ee539207450a59759a356f64562
alh40 <alexander.leaver@me.com>
2022-05-23 11:19:22 +0100

    commited import changes

--- MovieApp/src/main/java/controller/MovieController.java
+++ MovieApp/src/main/java/controller/MovieController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import repository.MovieRepository;
 
-import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
 
 @RestController
 public class MovieController {
