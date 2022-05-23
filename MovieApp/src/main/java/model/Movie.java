fd85e0dd2a3bcabf75b82b3ed8e05eb85fadbb85
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-23 11:40:02 +0100

    fixing venueController&repository

--- MovieApp/src/main/java/model/Movie.java
+++ MovieApp/src/main/java/model/Movie.java
@@ -19,8 +19,8 @@ public class Movie {
     @ManyToMany(cascade = {CascadeType.ALL})
     @JoinTable(
             name = "movie",
-            joinColumns = { @JoinColumn(name = "movie_id") },
-            inverseJoinColumns = { @JoinColumn(name = "venue_id") }
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "venue_id")}
     )
     @JsonIgnoreProperties(value = {"movies"})
     private Set<Venue> venues;
@@ -93,7 +93,4 @@ public class Movie {
     public void setVenues(Set<Venue> venues) {
         this.venues = venues;
     }
-=======
-public class Movie {
->>>>>>> origin/TimetableModel
 }
