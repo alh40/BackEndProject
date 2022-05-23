1fa1e73f15ccce0cde25ee15e1e7d435d09fdfe9
Carol Li <ziqili33@gmail.com>
2022-05-23 10:55:26 +0100

    getMapping done

--- MovieApp/src/main/java/controller/TimetableController.java
+++ MovieApp/src/main/java/controller/TimetableController.java
@@ -1,10 +1,14 @@
 package controller;
 
 
+import model.Timetable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repository.TimetableRepository;
+
+import java.util.List;
 
 @RestController
 public class TimetableController {
@@ -12,8 +16,13 @@ public class TimetableController {
     @Autowired
     private TimetableRepository timetableRepository;
 
-    @GetMapping
-    public
+    @GetMapping("/timetable")
+    public List<Timetable> findAllTimetable(){
+        return timetableRepository.findAll();
+    }
+
+    @PostMapping
+
 
 
 
