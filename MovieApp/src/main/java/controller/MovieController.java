f7046a86ab823ef2aea191374e493a4de2052610
alh40 <alexander.leaver@me.com>
2022-05-23 09:55:13 +0100

    INDEX request by title

--- MovieApp/src/main/java/controller/MovieController.java
+++ MovieApp/src/main/java/controller/MovieController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import repository.MovieRepository;
+
+@RestController
 public class MovieController {
+
+    @Autowired
+
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping("/movies/{title}")
+    public ResponseEntity<Movie> getByTitle(@PathVariable String movieTitle){
+        Movie movie1 = movieRepository.findbyTitle(title);
+        return ResponseEntity
+                .ok()
+                .body(movie1);
+    }
+
+
 }
