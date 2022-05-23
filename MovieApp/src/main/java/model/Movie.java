c3eaf49dd4f33a2bd72f1c1d0f3d42e8d314b9d8
hufengyi11 <fengyihu456@gmail.com>
2022-05-23 10:10:41 +0100

    added constructors, getters and setters

--- MovieApp/src/main/java/model/Movie.java
+++ MovieApp/src/main/java/model/Movie.java
@@ -27,5 +27,69 @@ public class Movie {
     public Movie() {
     }
 
+    public Movie(Long id, String title, int time_length, int rating, String genre, int price, Set<Venue> venues) {
+        this.id = id;
+        this.title = title;
+        this.time_length = time_length;
+        this.rating = rating;
+        this.genre = genre;
+        this.price = price;
+        this.venues = venues;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTime_length() {
+        return time_length;
+    }
+
+    public void setTime_length(int time_length) {
+        this.time_length = time_length;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Set<Venue> getVenues() {
+        return venues;
+    }
+
+    public void setVenues(Set<Venue> venues) {
+        this.venues = venues;
+    }
 }
