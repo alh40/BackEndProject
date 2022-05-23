7931e59e85fe79df2d389e9819cb669d22c1b6ce
Carol Li <ziqili33@gmail.com>
2022-05-23 11:09:39 +0100

    timetable controller - postmapping done

--- MovieApp/src/main/java/controller/TimetableController.java
+++ MovieApp/src/main/java/controller/TimetableController.java
@@ -1,13 +1,14 @@
 package controller;
 
 
+import model.Movie;
 import model.Timetable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import repository.TimetableRepository;
 
+import java.sql.Time;
 import java.util.List;
 
 @RestController
@@ -16,12 +17,21 @@ public class TimetableController {
     @Autowired
     private TimetableRepository timetableRepository;
 
-    @GetMapping("/timetable")
-    public List<Timetable> findAllTimetable(){
-        return timetableRepository.findAll();
+    @GetMapping("/timetable/{movie_name}")
+    public ResponseEntity<Timetable> getByTitle(@PathVariable String movie_name){
+        Timetable = timetableRepository.f(movie_name);
+        return ResponseEntity
+                .ok()
+                .body(movie);
     }
 
-    @PostMapping
+    @PostMapping("/timetable")
+    public ResponseEntity<Timetable> createStudent(@RequestBody Timetable timetable){
+        Timetable timetable1 = timetableRepository.save(timetable);
+        return ResponseEntity
+                .ok()
+                .body(timetable1);
+    }
 
 
 
--- MovieApp/src/main/java/repository/TimetableRepository.java
+++ MovieApp/src/main/java/repository/TimetableRepository.java
@@ -2,6 +2,17 @@ package repository;
 
 import model.Timetable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TimetableRepository extends JpaRepository<Timetable, Long> {
+
+    @Query(
+            value = "SELECT * FROM timetable WHERE  ",
+            nativeQuery = true)
+    List<Timetable>
+
+
+
 }
