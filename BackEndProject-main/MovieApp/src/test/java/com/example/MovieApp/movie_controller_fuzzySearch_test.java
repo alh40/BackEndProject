3fc10eee42dcd159b945074870be4b6c7c9a4d90
Fengyi Hu <fengyihu456@gmail.com>
2022-05-24 15:33:29 +0100

    added test

---
+++ BackEndProject-main/MovieApp/src/test/java/com/example/MovieApp/movie_controller_fuzzySearch_test.java
@@ -0,0 +1,24 @@
+package com.example.MovieApp;
+
+import com.example.MovieApp.model.Movie;
+import com.example.MovieApp.repository.MovieRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class movie_controller_fuzzySearch_test {
+    @Autowired
+    MovieRepository movieRepository;
+
+
+    @Test
+    public void canFindName(){
+        List<Movie> found = movieRepository.findByTitleLike("of");
+        assertThat(found.size()).isEqualTo(8);
+    }
+}
