ccc7d34fcd72a75bb20ad53ca720094f67ca03ab
Carol Li <ziqili33@gmail.com>
2022-05-25 15:55:02 +0100

    add put to timetable controller

--- BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/controller/TimetableController.java
+++ BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/controller/TimetableController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sound.midi.Soundbank;
+import java.sql.Time;
 import java.util.List;
 
 @RestController
@@ -77,6 +78,14 @@ public class TimetableController {
                 .ok()
                 .body(timetable1);
     }
+
+
+    @PutMapping(value = "/timetable/update/{id}", produces = {"application/json"})
+    public Timetable updateMovie(@RequestBody Timetable timetable, @PathVariable Long id){
+        return timetableRepository.save(timetable);
+    }
+
+
 
     @DeleteMapping("/timetable/{id}")
     public void deleteTimetable(@PathVariable Long id){
