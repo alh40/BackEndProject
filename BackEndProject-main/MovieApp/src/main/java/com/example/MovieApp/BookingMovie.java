0135eea03988b4c7a0d4333f59ab82a56d3b0cc2
alh40 <alexander.leaver@me.com>
2022-05-25 09:25:02 +0100

    finished booking methods

--- BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/BookingMovie.java
+++ BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/BookingMovie.java
@@ -2,11 +2,43 @@ package com.example.MovieApp;
 
 import com.example.MovieApp.controller.MovieController;
 import com.example.MovieApp.model.Movie;
+import com.example.MovieApp.model.Timetable;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class BookingMovie {
+
+    public static void writeToFile() {
+        try {
+            BookingMovie.writeFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<Integer> setUniqueId() {
+        LinkedHashSet<Integer> uniqueIdSet = new LinkedHashSet<>();
+        Random uniqueId = new Random();
+
+        while (uniqueIdSet.size() < 1000) {
+            uniqueIdSet.add(uniqueId.nextInt(1000 - 0 + 1));
+        }
+        ArrayList<Integer> uniqueIdList = new ArrayList<>();
+        uniqueIdList.addAll(uniqueIdSet);
+        return uniqueIdList;
+    }
+
+    public static ArrayList<Integer> uniqueId() {
+        ArrayList<Integer> userIdList = setUniqueId();
+        return userIdList;
+    }
+
+    public static ArrayList<Integer> uniqueIdUser = uniqueId();
+    public static List<Timetable> timetable = new ArrayList<>();
+    public static List<Movie> movieBooking = new ArrayList<>();
+    public static ArrayList<Timetable> newBooking = new ArrayList<>();
+    public static List<String> newList = new ArrayList<>();
 
     public static void writeFile() throws IOException {
 
@@ -19,8 +51,8 @@ public class BookingMovie {
         BufferedWriter writeBookings = new BufferedWriter(new FileWriter(bookings));
 
 
-        for(int i = 0; i < MovieController.newList.size(); i++){
-            writeBookings.write(MovieController.newList.get(i).toString());
+        for(int i = 0; i < newList.size(); i++){
+            writeBookings.write(newList.get(i).toString());
             writeBookings.newLine();
             writeBookings.newLine();
         }
--- BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/controller/MovieController.java
+++ BackEndProject-main/MovieApp/src/main/java/com/example/MovieApp/controller/MovieController.java
@@ -31,52 +31,23 @@ public class MovieController {
         this.movieRepository = movieRepository;
     }
 
-    public static void writeToFile() {
-        try {
-            BookingMovie.writeFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    public static ArrayList<Integer> setUniqueId() {
-        LinkedHashSet<Integer> uniqueIdSet = new LinkedHashSet<>();
-        Random uniqueId = new Random();
-
-        while (uniqueIdSet.size() < 1000) {
-            uniqueIdSet.add(uniqueId.nextInt(1000 - 0 + 1));
-        }
-        ArrayList<Integer> uniqueIdList = new ArrayList<>();
-        uniqueIdList.addAll(uniqueIdSet);
-        return uniqueIdList;
-    }
-
-    public static ArrayList<Integer> uniqueId() {
-        ArrayList<Integer> userIdList = setUniqueId();
-        return userIdList;
-    }
-
-    public static ArrayList<Integer> uniqueIdUser = uniqueId();
-    public static List<Timetable> timetable = new ArrayList<>();
-    public static List<Movie> movieBooking = new ArrayList<>();
-    public static ArrayList<Timetable> newBooking = new ArrayList<>();
-    public static List<String> newList = new ArrayList<>();
 
 
     @GetMapping(value = "/booking/{title}/{date}/{time}/{user}", produces = {"application/json"})
     public void bookMovie (@PathVariable String title, @PathVariable String time, @PathVariable String date, @PathVariable String user) throws IOException {
-        ArrayList<Integer> uniqueId = uniqueIdUser;
+        ArrayList<Integer> uniqueId = BookingMovie.uniqueIdUser;
         Movie movie1 = movieRepository.findByTitle(title);
         Set<Timetable> set = movie1.getTimetables();
-        timetable.addAll(set);
+        BookingMovie.timetable.addAll(set);
 
-        for(int i = 0; i < timetable.size(); i++){
-            if(timetable.get(i).getTime().equals(time) && timetable.get(i).getDate().equals(date)){
-                newBooking.add(timetable.get(i));
-                newList.add("User: " + user + " UniqueID: " + uniqueId.get(0) + ", " + "\n" + movie1.toString() + " " + "\n" + timetable.get(i).toString() + " " + "\n" + timetable.get(i).getVenue());
+        for(int i = 0; i < BookingMovie.timetable.size(); i++){
+            if(BookingMovie.timetable.get(i).getTime().equals(time) && BookingMovie.timetable.get(i).getDate().equals(date)){
+                BookingMovie.newBooking.add(BookingMovie.timetable.get(i));
+                BookingMovie.newList.add("User: " + user + " UniqueID: " + uniqueId.get(0) + ", " + "\n" + movie1.toString() + " " + "\n" + BookingMovie.timetable.get(i).toString() + " " + "\n" + BookingMovie.timetable.get(i).getVenue());
                 uniqueId.remove(0);
-                newBooking.remove(0);
-                writeToFile();
+                BookingMovie.newBooking.remove(0);
+                BookingMovie.writeFile();;
                 readFile();
             }
         }
@@ -89,12 +60,12 @@ public class MovieController {
     public void deleteMovieBookingByUniqueId(@PathVariable Long id) throws IOException {
         String integerToString = String.valueOf(id);
 
-        for(int i = 0; i < newList.size(); i++){
-            if(newList.get(i).contains(integerToString)){
-                newList.remove(i);
+        for(int i = 0; i < BookingMovie.newList.size(); i++){
+            if(BookingMovie.newList.get(i).contains(integerToString)){
+                BookingMovie.newList.remove(i);
                 File deleteFile = new File("/Users/Alex/Documents/MovieBookings.txt");
                 deleteFile.delete();
-                writeToFile();
+                BookingMovie.writeToFile();
                 readFile();
             }
         }
